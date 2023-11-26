1d5a0da13071531d99b336c20fa2dc0848102d73
lowpedrow <pedrohenriquep.motta@gmail.com>
2023-11-26 19:06:11 -0300

    alteracao na definicao

--- lib/definicoes.dart
+++ lib/definicoes.dart
@@ -6,33 +6,60 @@ class Definicoes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        onWillPop: () async {
-          // Navegar para a HomePage e bloquear a ação padrão de voltar
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => HomePage()),
-          );
-          return false;
-        },
-        child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: Text(
-              'Definições',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            backgroundColor: Color(0xFF750815),
-            iconTheme: IconThemeData(
-              color: Colors.white, // Defina a cor da seta de voltar aqui
+      onWillPop: () async {
+        // Navegar para a HomePage e bloquear a ação padrão de voltar
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'Definições',
+            style: TextStyle(
+              color: Colors.white,
             ),
           ),
-          body: Center(
-            child: Text('Conteúdo da tela de definições'),
+          backgroundColor: Color(0xFF750815),
+          iconTheme: IconThemeData(
+            color: Colors.white, // Defina a cor da seta de voltar aqui
           ),
-          bottomNavigationBar: Menu(),
-          backgroundColor: Color(0xFFFFD9DD),
-        ));
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              buildTextContainer('Usuário'),
+              buildTextContainer('Análise'),
+              buildTextContainer('Lembrete'),
+              buildTextContainer('Personalizar'),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Menu(),
+        backgroundColor: Color(0xFFFFD9DD),
+      ),
+    );
+  }
+
+  Widget buildTextContainer(String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(255, 238, 238, 1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 45,
+          color: Color.fromRGBO(117, 8, 21, 1),
+        ),
+      ),
+    );
   }
 }
--- lib/registro.dart
+++ lib/registro.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'menu.dart';
 import 'homepage.dart'; // Importe a HomePage
 
+
 class Registro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -28,11 +29,63 @@ class Registro extends StatelessWidget {
               color: Colors.white, // Defina a cor da seta de voltar aqui
             ),
           ),
-          body: Center(
-            child: Text('Conteúdo da tela de registro'),
+          body: 
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children:[
+              RowWithCircles(),
+              RowWithCircles(),
+              RowWithCircles(),
+            ],
+                      
           ),
+
           bottomNavigationBar: Menu(),
           backgroundColor: Color(0xFFFFD9DD),
         ));
+  }
+}
+
+class RowWithCircles extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        CircleWidget(),
+        CircleWidget(),
+        CircleWidget(),
+        CircleWidget(),
+        CircleWidget(),
+      ],
+    );
+  }
+}
+
+class CircleWidget extends StatefulWidget {
+  @override
+  _CircleWidgetState createState() => _CircleWidgetState();
+}
+
+class _CircleWidgetState extends State<CircleWidget> {
+  Color _circleColor = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _circleColor = _circleColor == Colors.blue ? Colors.white : Colors.blue;
+        });
+      },
+      child: Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: _circleColor,
+        ),
+      ),
+    );
   }
 }
